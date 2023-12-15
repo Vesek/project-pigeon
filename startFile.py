d19357b1537e5e56035cb39819c0d27dbb109d8d
MByteBandit <fiser.mi7@gmail.com>
2023-12-15 16:31:07 +0100

    Co-authored-by: XenoWhisperer <XenoWhisperer@users.noreply.github.com>

--- startFile.py
+++ startFile.py
@@ -8,8 +8,8 @@ variable = "value" #you dont tell a type of a variable here
 
 # step 2: functions
 
-def function_name():        #def operator sp
-    print("hello world")
+def function_name():        #def operator for specyfiing functions
+    print("hello world") #print function is used to write text to the consol
 
     
 
